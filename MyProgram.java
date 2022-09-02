3b035991f4c1eb68726096037ed914587fcbeb72
avikdasm99 <110466059+avikdasm99@users.noreply.github.com>
2022-09-02 14:17:08 +0530

    Add files via upload

---
+++ MyProgram.java
@@ -0,0 +1,15 @@
+package assign;
+
+class DispalyMassage{
+	void printMassage() {
+		System.out.println("Hellow my massage");
+	}
+}
+
+public class MyProgram {
+
+	public static void main(String[] args) {
+		DispalyMassage program=new DispalyMassage();
+		program.printMassage();
+	}
+}
