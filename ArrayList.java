6aa94795bccc4afdf10480ca09a3758d2d3a43a1
avikdasm99 <110466059+avikdasm99@users.noreply.github.com>
2022-09-28 07:55:14 +0530

    Add files via upload

---
+++ ArrayList.java
@@ -0,0 +1,14 @@
+/*2. Write a Java program to create a new array list, 
+ * add some colors (string) and print out the collection.*/
+
+package array;
+
+public class ArrayList {
+	public static void main(String[] args) {
+		
+	 String [] color = new String [] {"red","yellow","blue","black"} ;
+	 for(String i : color) {
+			System.out.println(i );
+          }
+	 }
+}
